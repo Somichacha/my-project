dddf5130e989b33236ecf4edbd633fdf46a35234
Zion <somistaste@gmail.com>
2026-06-08 13:11:12 +0100

    initial commit

---
+++ cypress/e2e/PageObjects/locators.js
@@ -0,0 +1,13 @@
+module.exports = {
+  URL: 'https://practice.qabrains.com',
+  submitbutton: '.btn-submit',
+  registrationbutton: '#registration > .text',
+  namefield: '[name="name"]',
+  selectcountryfield: '[name="country"]',
+  selectaccountfield: '[name="account"]',
+  emailfield: '[name="email"]',
+  passwordfield: '[name="password"]',
+  confirmpasswordfield: '[name="confirm_password"]',
+  signupbutton: 'form > .whitespace-nowrap',
+  successmessage: '.success-msg > .text-xl',
+}
